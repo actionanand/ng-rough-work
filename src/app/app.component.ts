ca29a75fcaafcb9385b538fe23390dff33df57cf
actionanand <meetanandns@gmail.com>
2020-04-14 22:53:20 +0530

    formBuilder used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,12 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'ng-test';
+  title = 'ng-Rough work';
   myForm: FormGroup;
   genderNames = ['Male', 'Female', 'Not prefer to tell'];
   hobbyArray: string[] = null;
+
+  //check box
   fruits: Array<any> = [
     {name: 'Banana', value: 'banana'},
     {name: 'Apple', value: 'apple'},
@@ -21,19 +23,29 @@ export class AppComponent implements OnInit {
     {name: 'Orange', value: 'orange'},
   ];
   fruitAr: FormArray;
+  favFruits: string[] = null;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.myForm = new FormGroup({
-      userData: new FormGroup({
-        userName: new FormControl(null, Validators.required),
-        bioData: new FormControl(null, [Validators.required, Validators.minLength(5)])
-      }),
-      gender: new FormControl('Male'),
-      hobbies: new FormArray([]),
-      checkBox: new FormArray([], Validators.required)
+    // this.myForm = new FormGroup({
+    //   userData: new FormGroup({
+    //     userName: new FormControl(null, Validators.required),
+    //     bioData: new FormControl(null, [Validators.required, Validators.minLength(5)])
+    //   }),
+    //   gender: new FormControl('Male'),
+    //   hobbies: new FormArray([]),
+    //   checkBox: new FormArray([], Validators.required)
+    // });
 
+    this.myForm = this.fb.group({
+      userData: this.fb.group({
+        userName: this.fb.control(null, Validators.required),
+        bioData: this.fb.control(null, [Validators.required, Validators.minLength(5)])
+      }),
+      gender: this.fb.control('Female'),
+      hobbies: this.fb.array([]),
+      checkBox: this.fb.array([], Validators.required)
     });
   }
 
@@ -42,6 +54,8 @@ export class AppComponent implements OnInit {
     console.log(this.myForm.get('userData.bioData').value);
     console.log(this.myForm.get('gender').value);
     this.hobbyArray = this.myForm.get('hobbies').value;
+    this.favFruits = this.myForm.get('checkBox').value;
+    
     this.hobbyArray.forEach(a=> {
       console.log(a);
     });
@@ -60,13 +74,19 @@ export class AppComponent implements OnInit {
     if(e.target.checked) {
       this.fruitCheckboxAr.push(new FormControl(e.target.value));
     } else {
-      let i: number = 0;
+
+      // let i: number = 0;
+      // this.fruitCheckboxAr.controls.forEach((item: FormControl) => {
+      //   if (item.value == e.target.value) {
+      //     this.fruitCheckboxAr.removeAt(i);
+      //     return;
+      //   }
+      //   i++;
+      // });
+
       this.fruitCheckboxAr.controls.forEach((item: FormControl) => {
-        if (item.value == e.target.value) {
-          this.fruitCheckboxAr.removeAt(i);
-          return;
-        }
-        i++;
+        let index = this.fruitCheckboxAr.controls.indexOf(item);
+        this.fruitCheckboxAr.removeAt(index);
       });
     }
   }
